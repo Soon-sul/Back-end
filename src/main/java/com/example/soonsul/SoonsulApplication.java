d82e15a27ba00b8f508b58edde9ec2d1b1337ff2
Junhui0u0 <qaz74792@gmail.com>
2023-07-21 15:21:34 +0900

    feat : 로그 설정

--- src/main/java/com/example/soonsul/SoonsulApplication.java
+++ src/main/java/com/example/soonsul/SoonsulApplication.java
@@ -5,13 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SoonsulApplication {
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.properties ,"
 			+ "classpath:oauth.yml";
 
+
 	public static void main(String[] args) {
+
+		System.setProperty("logging.file.name", "/home/ubuntu/Back-end/src/main/resources/static/"
+				+ new SimpleDateFormat("yyyy-MM-dd hh:mm").format(new Date())+".log");
+
 		new SpringApplicationBuilder(SoonsulApplication.class)
 				.properties(APPLICATION_LOCATIONS)
 				.run(args);
