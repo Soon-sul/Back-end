98a8ffd362d11ebcea70d69b4d1de5fa53e1de48
Junhui0u0 <qaz74792@gmail.com>
2023-07-23 01:53:46 +0900

    refactor : 주석처리

--- src/main/java/com/example/soonsul/SoonsulApplication.java
+++ src/main/java/com/example/soonsul/SoonsulApplication.java
@@ -18,11 +18,12 @@ public class SoonsulApplication {
 			+ "classpath:application.properties ,"
 			+ "classpath:oauth.yml";
 
+	/*
 	@PostConstruct
 	public void started() {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
-
+	*/
 
 	public static void main(String[] args) {
 		System.setProperty("logging.file.name", "/home/ubuntu/log/"
