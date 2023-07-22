d013b18c8068eec4370ad3373ec6e373e906b2aa
Junhui0u0 <qaz74792@gmail.com>
2023-07-23 02:51:28 +0900

    refactor : 로그 경로 변경

--- src/main/java/com/example/soonsul/SoonsulApplication.java
+++ src/main/java/com/example/soonsul/SoonsulApplication.java
@@ -1,17 +1,12 @@
 package com.example.soonsul;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-import javax.annotation.PostConstruct;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.Date;
-import java.util.TimeZone;
+
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SoonsulApplication {
@@ -19,8 +14,9 @@ public class SoonsulApplication {
 			+ "classpath:application.properties ,"
 			+ "classpath:oauth.yml";
 
+
 	public static void main(String[] args) {
-		System.out.println(LocalDateTime.now(ZoneId.of("Asia/Seoul")));
+
 		System.setProperty("logging.file.name", "/home/ubuntu/log/"
 				+ LocalDate.now(ZoneId.of("Asia/Seoul"))+".log");
 
