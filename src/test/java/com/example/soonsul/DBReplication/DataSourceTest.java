79a1951a5398cf4209d82ad7b381a7d05ed0bafa
Junhui0u0 <qaz74792@gmail.com>
2023-09-01 00:43:04 +0900

    test : DB Replication 테스트
    데이터소스 생성 및 @Transactional(readOnly=true/false)에 따른 dataSourceType 확인 기능을 테스트했습니다.

---
+++ src/test/java/com/example/soonsul/DBReplication/DataSourceTest.java
@@ -0,0 +1,61 @@
+package com.example.soonsul.DBReplication;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(properties = "spring.config.location="
+        + "classpath:application.properties ,"
+        + "classpath:oauth.yml"
+)
+public class DataSourceTest {
+
+    @Autowired
+    Environment environment;
+
+
+    @Test
+    void Master_DataSource_테스트(@Qualifier("masterDataSource") final DataSource masterDataSource) {
+        //given
+        String url = environment.getProperty("spring.datasource.master.hikari.jdbc-url");
+        String username = environment.getProperty("spring.datasource.master.hikari.username");
+        String driverClassName = environment.getProperty("spring.datasource.master.hikari.driver-class-name");
+        Boolean readOnly = Boolean.valueOf(environment.getProperty("spring.datasource.master.hikari.read-only"));
+
+        //when
+        HikariDataSource hikariDataSource = (HikariDataSource) masterDataSource;
+
+        //then
+        assertThat(hikariDataSource.isReadOnly()).isEqualTo(readOnly);
+        assertThat(hikariDataSource.getJdbcUrl()).isEqualTo(url);
+        assertThat(hikariDataSource.getUsername()).isEqualTo(username);
+        assertThat(hikariDataSource.getDriverClassName()).isEqualTo(driverClassName);
+    }
+
+
+    @Test
+    void Slave_DataSource_테스트(@Qualifier("slaveDataSource") final DataSource slaveDataSource) {
+        //given
+        String url = environment.getProperty("spring.datasource.slave.hikari.jdbc-url");
+        String username = environment.getProperty("spring.datasource.slave.hikari.username");
+        String driverClassName = environment.getProperty("spring.datasource.slave.hikari.driver-class-name");
+        Boolean readOnly = Boolean.valueOf(environment.getProperty("spring.datasource.slave.hikari.read-only"));
+
+        //when
+        HikariDataSource hikariDataSource = (HikariDataSource) slaveDataSource;
+
+        //then
+        assertThat(hikariDataSource.isReadOnly()).isEqualTo(readOnly);
+        assertThat(hikariDataSource.getJdbcUrl()).isEqualTo(url);
+        assertThat(hikariDataSource.getUsername()).isEqualTo(username);
+        assertThat(hikariDataSource.getDriverClassName()).isEqualTo(driverClassName);
+    }
+
+}
---
+++ src/test/java/com/example/soonsul/DBReplication/ReplicationTest.java
@@ -0,0 +1,56 @@
+package com.example.soonsul.DBReplication;
+
+import com.example.soonsul.DBReplication.common.DataSourceType;
+import com.example.soonsul.DBReplication.replication.ReplicationRoutingDataSource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest(properties = "spring.config.location="
+        + "classpath:application.properties ,"
+        + "classpath:oauth.yml"
+)
+public class ReplicationTest {
+
+    private static final String Test_Method_Name = "determineCurrentLookupKey";
+
+
+    @Test
+    @Transactional
+    void 쓰기전용_트랜잭션() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        //given
+        ReplicationRoutingDataSource replicationRoutingDataSource = new ReplicationRoutingDataSource();
+
+        //when
+        Method determineCurrentLookupKey = ReplicationRoutingDataSource.class.getDeclaredMethod(Test_Method_Name);
+        determineCurrentLookupKey.setAccessible(true);
+
+        DataSourceType dataSourceType = (DataSourceType) determineCurrentLookupKey.invoke(replicationRoutingDataSource);
+
+        //then
+        assertEquals(dataSourceType, DataSourceType.Master);
+    }
+
+
+    @Test
+    @Transactional(readOnly = true)
+    void 읽기전용_트랜잭션() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        //given
+        ReplicationRoutingDataSource replicationRoutingDataSource = new ReplicationRoutingDataSource();
+
+        //when
+        Method determineCurrentLookupKey = ReplicationRoutingDataSource.class.getDeclaredMethod(Test_Method_Name);
+        determineCurrentLookupKey.setAccessible(true);
+
+        DataSourceType dataSourceType = (DataSourceType) determineCurrentLookupKey.invoke(replicationRoutingDataSource);
+
+        //then
+        assertEquals(dataSourceType, DataSourceType.Slave);
+    }
+
+}
