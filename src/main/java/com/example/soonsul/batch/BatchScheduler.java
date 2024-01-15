e4546cb94a944816d06b3c9eb3c272902e0c8518
jodonghyeon3 <jawoo1003@gmail.com>
2024-01-15 20:00:12 +0900

    Fix: BatchConfig 주석해제 및 CI/CD Test

--- src/main/java/com/example/soonsul/batch/BatchScheduler.java
+++ src/main/java/com/example/soonsul/batch/BatchScheduler.java
@@ -8,7 +8,6 @@ import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -31,7 +30,8 @@ public class BatchScheduler {
         try {
             jobLauncher.run(batchConfig.liquorBatchJob(), jobParameters);
         } catch (JobExecutionAlreadyRunningException | JobInstanceAlreadyCompleteException
-                 | JobParametersInvalidException | org.springframework.batch.core.repository.JobRestartException e) {
+                 | JobParametersInvalidException |
+                 org.springframework.batch.core.repository.JobRestartException e) {
 
             log.error(e.getMessage());
         }
