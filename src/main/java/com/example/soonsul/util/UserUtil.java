5ef9b4a38a9cd655e3f28632f8443e484d193c20
jodonghyeon3 <jawoo1003@gmail.com>
2024-01-29 20:52:47 +0900

    [Fix] 유저정보 로그확인 코드 작성

--- src/main/java/com/example/soonsul/util/UserUtil.java
+++ src/main/java/com/example/soonsul/util/UserUtil.java
@@ -5,19 +5,28 @@ import com.example.soonsul.user.entity.User;
 import com.example.soonsul.user.exception.UserNotExist;
 import com.example.soonsul.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class UserUtil {
     private final UserRepository userRepository;
 
     public User getUserByAuthentication(){
         Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
-        return userRepository.findById(authentication.getName())
-                .orElseThrow(()-> new UserNotExist("login user not exist", ErrorCode.USER_NOT_EXIST));
+        try {
+            return userRepository.findById(authentication.getName())
+                    .orElseThrow(() -> new UserNotExist("login user not exist", ErrorCode.USER_NOT_EXIST));
+        } catch (Exception e) {
+            log.error(e + "유저인증");
+            throw new RuntimeException("");
+        }
+
+
     }
 
     public User getUserById(String userId){
